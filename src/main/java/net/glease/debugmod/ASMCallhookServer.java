24473b093a13f4e0205494e122918454679c76a1
Glease <4586901+glease@users.noreply.github.com>
2021-06-03 22:52:19 +0800

    change how thread dump is added to crash report

--- src/main/java/net/glease/debugmod/ASMCallhookServer.java
+++ src/main/java/net/glease/debugmod/ASMCallhookServer.java
@@ -4,16 +4,14 @@ import net.minecraft.crash.CrashReport;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
-import java.util.Arrays;
 import java.util.ConcurrentModificationException;
-import java.util.stream.Collectors;
 
 public class ASMCallhookServer {
+    static ThreadInfo[] dump;
     @Callhook
     public static void enhanceCrashReport(CrashReport cr) {
         if (cr.getCrashCause() instanceof ConcurrentModificationException) {
-            ThreadInfo[] threads = ManagementFactory.getThreadMXBean().dumpAllThreads(true, true);
-            cr.getCategory().addCrashSection("Thread dump", Arrays.stream(threads).map(ThreadInfo::toString).collect(Collectors.joining("","\n","")));
+            dump = ManagementFactory.getThreadMXBean().dumpAllThreads(true, true);
         }
     }
 }
--- src/main/java/net/glease/debugmod/LoadingPlugin.java
+++ src/main/java/net/glease/debugmod/LoadingPlugin.java
@@ -1,9 +1,16 @@
 package net.glease.debugmod;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.ICrashCallable;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 import java.io.File;
+import java.lang.management.ThreadInfo;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static net.glease.debugmod.ASMCallhookServer.dump;
 
 @IFMLLoadingPlugin.Name("DebugMod")
 public class LoadingPlugin implements IFMLLoadingPlugin {
@@ -31,6 +38,20 @@ public class LoadingPlugin implements IFMLLoadingPlugin {
         debugOutputDir = new File((File) data.get("mcLocation"), ".asm");
         //noinspection ResultOfMethodCallIgnored
         debugOutputDir.mkdir();
+        FMLCommonHandler.instance().registerCrashCallable(new ICrashCallable() {
+            @Override
+            public String getLabel() {
+                return "Thread dump";
+            }
+
+            @Override
+            public String call() {
+                if (dump == null) {
+                    return "Not generated";
+                }
+                return Arrays.stream(dump).map(ThreadInfo::toString).collect(Collectors.joining("", "\n", ""));
+            }
+        });
     }
 
     @Override
