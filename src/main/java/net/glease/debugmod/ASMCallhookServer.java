607c44842ff28d68425427025434b59beda7fa16
Glease <4586901+glease@users.noreply.github.com>
2021-06-04 14:51:48 +0800

    Try even harder to determine if it's CME

--- src/main/java/net/glease/debugmod/ASMCallhookServer.java
+++ src/main/java/net/glease/debugmod/ASMCallhookServer.java
@@ -1,21 +1,27 @@
 package net.glease.debugmod;
 
-import net.minecraft.crash.CrashReport;
-
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
+import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 
 public class ASMCallhookServer {
     static ThreadInfo[] dump;
     @Callhook
     public static void onCrashReportCreated(Throwable throwable) {
-        if (throwable instanceof ConcurrentModificationException) {
+        if (isCME(throwable)) {
             dump = ManagementFactory.getThreadMXBean().dumpAllThreads(true, true);
         } else {
             // for whatever reason the previous crash report was discarded and didn't crash the whole thing
             // so we should clear it up.
             dump = null;
         }
+    }
+
+    private static boolean isCME(Throwable t) {
+        Throwable ex = t;
+        while (ex != null && !(ex instanceof ConcurrentModificationException) && Arrays.stream(t.getSuppressed()).noneMatch(ASMCallhookServer::isCME))
+            ex = ex.getCause();
+        return ex != null;
     }
 }
